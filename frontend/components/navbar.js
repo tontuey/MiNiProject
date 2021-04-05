7373f22c5310b6213899425f5829c85c332bab97
Papawin <tueyconan@hotmail.co.th>
2021-04-05 16:25:17 +0700

    check token

---
+++ frontend/components/navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const Navbar = () => (
+    <div>
+        <Link href="/"><a> Home </a></Link> |
+        <Link href="/register"><a> Register </a></Link>  |
+        <Link href="/login"><a> Login </a></Link> |
+        <Link href="/profile"><a> Profile </a></Link> | 
+        <Link href="/getConfig"><a> Config </a></Link> | 
+        <Link href="/logout"><a> Logout </a></Link> |
+        <Link href="/foo"><a> Foo </a></Link> 
+
+    </div>
+)
+
+export default Navbar
